a143053db3b5f3ca107e50385f43bc68d9dc334f
fedenadal <fedendl@gmail.com>
2022-07-25 23:20:03 -0300

    git

---
+++ codigo.js
@@ -0,0 +1,11 @@
+let editable = document.querySelector (".editable");
+editable.setAttribute ("contentEditable", "true" );
+
+let input = document.querySelector(".input-normal")
+document.write(input.value);
+input.type="color"
+
+let estilizable = document.querySelector(".estilizable");
+estilizable.style.color= "red"
+
+let agregaleClases
