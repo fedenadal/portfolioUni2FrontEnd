4afe779fb9e06f5b0562c02984c8769732331f7c
fedenadal <104152035+fedenadal@users.noreply.github.com>
2023-01-12 16:09:12 -0300

    Update experiencia.service.ts
    corrección CORS a railway

--- src/app/servicios/experiencia.service.ts
+++ src/app/servicios/experiencia.service.ts
@@ -7,7 +7,7 @@ import { Experiencia } from '../model/experiencia';
   providedIn: 'root'
 })
 export class ExperienciaService {
-expURL ='http://localhost:8080/explab/'
+expURL ='portfoliobackend-production-1b35.up.railway.app/explab/'
 
 constructor(private httpClient: HttpClient) { }
 
