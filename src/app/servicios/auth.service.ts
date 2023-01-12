9f41449ce9cec758c9c69f5f39542aa17d610843
fedenadal <104152035+fedenadal@users.noreply.github.com>
2023-01-12 16:06:28 -0300

    Update auth.service.ts
    correcion de cors a railway

--- src/app/servicios/auth.service.ts
+++ src/app/servicios/auth.service.ts
@@ -9,7 +9,7 @@ import { NuevoUsuario } from '../model/nuevo-usuario';
   providedIn: 'root'
 })
 export class AuthService {
-  authURL='http://localhost:8080/auth/';
+  authURL='portfoliobackend-production-1b35.up.railway.app/auth/';
 
   constructor(private httpClient: HttpClient) { }
 
